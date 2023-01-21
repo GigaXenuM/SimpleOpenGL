6884a7adaf391dc51dff6a85a6c3b1e6044d1cf9
Taras Kachmaryk <taras.kachmaryk.wt@renesas.com>
2023-01-21 14:27:35 +0200

    Add camera implementation

--- app/src/scene/camera/camera.cpp
+++ app/src/scene/camera/camera.cpp
@@ -1,6 +1,48 @@
 #include "camera.h"
 
-Camera::Camera()
-{
+#include "glm/ext/matrix_clip_space.hpp"
+#include "glm/ext/matrix_transform.hpp"
 
+Scene::Camera::Camera(double fov, double aspectRatio, double renderDistance)
+    : _fov{ fov },
+      _aspectRatio{ aspectRatio },
+      _renderDistance{ renderDistance },
+      _position{ 0.0 },
+      _cameraFront{ 0.0, 0.0, -1.0 },
+      _direction{ glm::normalize(_position - _cameraFront) },
+      _up{ 0.0, 1.0, 0.0 },
+      _cameraRight{ glm::normalize(glm::cross(_up, _direction)) },
+      _cameraUp{ glm::cross(_direction, _cameraRight) },
+      _projection{ glm::perspective(glm::radians(_fov), _aspectRatio, 0.1, _renderDistance) },
+      _view{ glm::lookAt(_position, _position + _cameraFront, _cameraUp) }
+{
+}
+
+void Scene::Camera::setPosition(glm::vec3 position)
+{
+    _position = position;
+    update();
+}
+
+glm::vec3 Scene::Camera::position()
+{
+    return _position;
+}
+
+glm::mat4 Scene::Camera::projection()
+{
+    return _projection;
+}
+
+glm::mat4 Scene::Camera::view()
+{
+    return _view;
+}
+
+void Scene::Camera::update()
+{
+    _direction = glm::normalize(_position - _cameraFront);
+    _cameraRight = glm::normalize(glm::cross(_up, _direction));
+    _cameraUp = glm::cross(_direction, _cameraRight);
+    _view = glm::lookAt(_position, _position + _cameraFront, _cameraUp);
 }
--- app/src/scene/camera/camera.h
+++ app/src/scene/camera/camera.h
@@ -1,8 +1,39 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
+namespace Scene
+{
+
 class Camera
 {
 public:
-    Camera();
-    ~Camera();
+    Camera(double fov, double aspectRatio, double renderDistance);
+
+    void setPosition(glm::vec3 position);
+    glm::vec3 position();
+
+    glm::mat4 projection();
+    glm::mat4 view();
+
+private:
+    void update();
+    void updateDirection();
+    void updateView();
+
+    double _fov;
+    double _aspectRatio;
+    double _renderDistance;
+
+    glm::vec3 _position;
+    glm::vec3 _cameraFront;
+    glm::vec3 _direction;
+
+    glm::vec3 _up;
+    glm::vec3 _cameraRight;
+    glm::vec3 _cameraUp;
+
+    glm::mat4 _projection;
+    glm::mat4 _view;
 };
+} // namespace Scene
